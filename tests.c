12ead62ebb4e777b6a3ed2f15e1b28e108959c72
Reuben Dunnington <reuben.dunnington@gmail.com>
2017-11-10 20:25:16 -0800

    log tests

--- tests.c
+++ tests.c
@@ -5,6 +5,18 @@
 
 #include "rjd_all.h"
 
+////////////////////////////////////////////////////////////////////////////////
+// expect utils
+
+void expect_str(const char* expected, const char* actual)
+{
+	if (expected != actual && strcmp(expected, actual)) {
+		printf("Expected:\n%s\nbut got:\n%s\n", expected, actual);
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
 char logbuffer[1024 * 128];
 size_t logbuffer_pos = 0;
 void test_log_hook(const char* msg, size_t length)
@@ -12,6 +24,7 @@ void test_log_hook(const char* msg, size_t length)
 	UNUSED_PARAM(length);
 
 	strncpy(logbuffer + logbuffer_pos, msg, sizeof(logbuffer) - logbuffer_pos);
+	logbuffer_pos += length;
 }
 
 void test_logging()
@@ -20,20 +33,64 @@ void test_logging()
 		.name = "Test Channel",
 		.enabled = true,
 		.hook = test_log_hook,
+		.verbosity = RJD_LOG_VERBOSITY_MED,
 	};
 	g_rjd_global_logchannel = &local;
 
 	// normal tests
-	LOG(
+	LOG("test");
+	LOG("");
+	LOG("%s%d%d%s", "forma", 1, 1, "ed!");
 
 	// disabled
+	local.enabled = false;
+	LOG("shouldn't get printed");
+	local.enabled = true;
 
-	// verbose
+	// verbosity
+	local.verbosity = RJD_LOG_VERBOSITY_LOW;
+	LOG_CHANNEL(&local, RJD_LOG_VERBOSITY_LOW, "ok1");
+	LOG_CHANNEL(&local, RJD_LOG_VERBOSITY_MED, "nope");
+	LOG_CHANNEL(&local, RJD_LOG_VERBOSITY_HIGH, "nope");
+
+	local.verbosity = RJD_LOG_VERBOSITY_MED;
+	LOG_CHANNEL(&local, RJD_LOG_VERBOSITY_LOW, "ok2");
+	LOG_CHANNEL(&local, RJD_LOG_VERBOSITY_MED, "ok2");
+	LOG_CHANNEL(&local, RJD_LOG_VERBOSITY_HIGH, "nope");
+
+	local.verbosity = RJD_LOG_VERBOSITY_HIGH;
+	LOG_CHANNEL(&local, RJD_LOG_VERBOSITY_LOW, "ok3");
+	LOG_CHANNEL(&local, RJD_LOG_VERBOSITY_MED, "ok3");
+	LOG_CHANNEL(&local, RJD_LOG_VERBOSITY_HIGH, "ok3");
+
+	local.verbosity = RJD_LOG_VERBOSITY_MED;
 
 	// other channel
+	struct rjd_logchannel local2 = {
+		.name = "Test2",
+		.enabled = true,
+		.hook = test_log_hook,
+		.verbosity = RJD_LOG_VERBOSITY_MED,
+	};
+
+	LOG_CHANNEL(&local2, RJD_LOG_VERBOSITY_MED, "other channel");
 
 	// expect equals
+	const char* expected = 
+		"tests.c(41): test\n"
+		"tests.c(42): \n"
+		"tests.c(43): forma11ed!\n"
+		"tests.c(52): ok1\n"
+		"tests.c(57): ok2\n"
+		"tests.c(58): ok2\n"
+		"tests.c(62): ok3\n"
+		"tests.c(63): ok3\n"
+		"tests.c(64): ok3\n"
+		"tests.c(76): other channel\n";
+
+	expect_str(expected, logbuffer);
 	
+	// reset global logger for future tests
 	rjd_log_resetglobal();
 }
 
@@ -70,9 +127,9 @@ void test_array()
 
 int main(void) 
 {
-	void test_logging();
-	void test_alloc();
-	void test_array();
+	test_logging();
+	test_alloc();
+	test_array();
 
 	return 0;
 }
