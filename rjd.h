c56c23139c0a8f69c1815e04b1ab150e98d7e760
Reuben Dunnington <reuben.dunnington@gmail.com>
2018-04-05 21:54:18 -0700

    updated rjd.h

--- rjd.h
+++ rjd.h
@@ -59,6 +59,28 @@
 #endif
 
 #if RJD_COMPILER_MSVC
+	#if defined(_M_X86) || defined(_M_X64)
+		#define RJD_ARCH_64 1
+	#elif defined(_M_IX86)
+		#define RJD_ARCH_32 1
+	#else
+		#error Unknown architecture
+	#endif
+#elif RJD_COMPILER_GCC
+	#if defined(__x86_64) || defined(__x86_64__)
+		#define RJD_ARCH_64 1
+	#elif defined(i386) || defined(__i386) || defined(__i386__)
+		#define RJD_ARCH_32 1
+	#else
+		#error Unknown architecture
+	#endif
+#elif RJD_COMPILER_CLANG
+	#error TODO: Implement for clang
+#else
+	#error Unhandled compiler
+#endif
+
+#if RJD_COMPILER_MSVC
 	#define RJD_COMPILER_MSVC_ONLY(code) code
 	#define RJD_COMPILER_GCC_ONLY(code)
 	#define RJD_COMPILER_CLANG_ONLY(code)
@@ -79,6 +101,20 @@
 #if RJD_ENABLE_SHORTNAMES
 	#define FORCE_INLINE RJD_FORCE_INLINE
 	#define FORCE_ALIGN RJD_FORCE_ALIGN
+
+	#define PLATFORM_WINDOWS	RJD_PLATFORM_WINDOWS
+	#define PLATFORM_OSX		RJD_PLATFORM_OSX
+
+	#define ARCH_64				RJD_ARCH_64
+	#define ARCH_32				RJD_ARCH_32
+
+	#define COMPILER_MSVC		RJD_COMPILER_MSVC
+	#define COMPILER_GCC        RJD_COMPILER_GCC
+	#define COMPILER_CLANG      RJD_COMPILER_CLANG
+
+	#define COMPILER_MSVC_ONLY	RJD_COMPILER_MSVC_ONLY
+	#define COMPILER_GCC_ONLY	RJD_COMPILER_GCC_ONLY
+	#define COMPILER_CLANG_ONLY	RJD_COMPILER_CLANG_ONLY
 #endif
 
 
