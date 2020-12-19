ac93ec2f394eeda841afa8f41fad0e17c1c6effe
Reuben Dunnington <reuben.dunnington@gmail.com>
2020-12-18 18:12:46 -0800

    gen.py script writes LF for newline on all platforms

--- gen.py
+++ gen.py
@@ -78,7 +78,7 @@ for filename in files:
         concat += data;
         concat += "\n"
 
-with open("rjd.h", "w") as file:
+with open("rjd.h", "w", newline='\n') as file:
     file.write(concat)
 
 with open("rjd_all.h", "w") as file:
